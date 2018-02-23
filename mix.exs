d43dbd2fa9d9dd82fadd62db54525c4418ec4697
Jan Fornoff <jan.fornoff@grandcentrix.net>
2018-02-23 15:57:19 +0100

    Update commanded dependency to Github master

--- mix.exs
+++ mix.exs
@@ -6,21 +6,21 @@ defmodule Commanded.EventStore.Adapters.Extreme.Mixfile do
       app: :commanded_extreme_adapter,
       version: "0.4.0",
       elixir: "~> 1.5",
-      elixirc_paths: elixirc_paths(Mix.env),
+      elixirc_paths: elixirc_paths(Mix.env()),
       description: description(),
       package: package(),
-      build_embedded: Mix.env == :prod,
-      start_permanent: Mix.env == :prod,
-      deps: deps(),
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
     ]
   end
 
   def application do
     [
       extra_applications: [
-        :logger,
+        :logger
       ],
-      mod: {Commanded.EventStore.Adapters.Extreme.Application, []},
+      mod: {Commanded.EventStore.Adapters.Extreme.Application, []}
     ]
   end
 
@@ -29,32 +29,39 @@ defmodule Commanded.EventStore.Adapters.Extreme.Mixfile do
 
   defp deps do
     [
-      {:commanded, ">= 0.15.0", runtime: false},
-      {:docker, github: "bearice/elixir-docker", tag: "03809fc594b9706c106fc28b7ef03c2dbde2fe93", only: :test},
+      {:commanded, github: "commanded/commanded", runtime: false},
+      {:docker,
+       github: "bearice/elixir-docker",
+       tag: "03809fc594b9706c106fc28b7ef03c2dbde2fe93",
+       only: :test},
       {:extreme, "~> 0.10"},
       {:ex_doc, "~> 0.15", only: :dev},
       {:hackney, "~> 1.8.0", override: true},
       {:httpoison, "~> 0.11.1"},
-      {:mix_test_watch, "~> 0.2", only: :dev},
+      {:mix_test_watch, "~> 0.2", only: :dev}
     ]
   end
 
-
   defp description do
-"""
-Extreme event store adapter for Commanded
-"""
+    """
+    Extreme event store adapter for Commanded
+    """
   end
 
   defp package do
     [
       files: [
-        "lib", "mix.exs", "README*", "LICENSE*",
+        "lib",
+        "mix.exs",
+        "README*",
+        "LICENSE*"
       ],
       maintainers: ["Ben Smith"],
       licenses: ["MIT"],
-      links: %{"GitHub" => "https://github.com/commanded/commanded-extreme-adapter",
-               "Docs" => "https://hexdocs.pm/commanded_extreme_adapter/"}
+      links: %{
+        "GitHub" => "https://github.com/commanded/commanded-extreme-adapter",
+        "Docs" => "https://hexdocs.pm/commanded_extreme_adapter/"
+      }
     ]
   end
 end
