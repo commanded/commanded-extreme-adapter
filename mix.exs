a3a148734eec5b4e38c9c3b038512fc49d0fa7a7
Alexander Borovykh <immaculate.pine@gmail.com>
2018-10-21 17:56:44 +0200

    Use master branch of the commanded library

--- mix.exs
+++ mix.exs
@@ -31,7 +31,8 @@ defmodule Commanded.EventStore.Adapters.Extreme.Mixfile do
 
   defp deps do
     [
-      {:commanded, ">= 0.16.0", runtime: false},
+      # @todo Lock to the next version of commanded when it is released
+      {:commanded, github: "commanded/commanded", runtime: false},
       {:extreme, "~> 0.11"},
       {:hackney, "~> 1.8.0", override: true},
       {:httpoison, "~> 0.11.1"},
