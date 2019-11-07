925ad5729b4e358a3c9ad76a3ef4ee3a881d49f8
Ben Smith <ben@10consulting.com>
2019-11-07 20:35:19 +0000

    Wait for up to one minute for the Event Store to become available to test

--- test/support/storage.ex
+++ test/support/storage.ex
@@ -1,10 +1,24 @@
 defmodule Commanded.EventStore.Adapters.Extreme.Storage do
-  def wait_for_event_store do
+  def wait_for_event_store(timeout \\ 5_000)
+
+  def wait_for_event_store(timeout) when is_integer(timeout) do
+    task = Task.async(&event_store_check/0)
+
+    case Task.yield(task, timeout) || Task.shutdown(task) do
+      {:ok, result} ->
+        result
+
+      nil ->
+        {:error, :timeout}
+    end
+  end
+
+  defp event_store_check do
     headers = [Accept: "application/vnd.eventstore.atom+json"]
     options = [recv_timeout: 400]
 
-    case HTTPoison.get("http://localhost:2113/streams/somestream", headers, options) do
-      {:ok, %HTTPoison.Response{status_code: 404}} ->
+    case HTTPoison.get("http://localhost:2113/web/index.html", headers, options) do
+      {:ok, %HTTPoison.Response{status_code: 200}} ->
         :timer.sleep(1_000)
 
         :ok
@@ -12,7 +26,7 @@ defmodule Commanded.EventStore.Adapters.Extreme.Storage do
       _ ->
         :timer.sleep(1_000)
 
-        wait_for_event_store()
+        event_store_check()
     end
   end
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,3 +1,5 @@
-Commanded.EventStore.Adapters.Extreme.Storage.wait_for_event_store()
+alias Commanded.EventStore.Adapters.Extreme.Storage
+
+:ok = Storage.wait_for_event_store(:timer.minutes(1))
 
 ExUnit.start()
